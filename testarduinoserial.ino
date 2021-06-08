f1a8ec9fc734e82dd9a7be58cf0ad1dd5b1d1b3d
Basil Eldho <37305148+Beta1313@users.noreply.github.com>
2021-06-09 01:57:49 +0530

    Update testarduinoserial.ino

--- testarduinoserial.ino
+++ testarduinoserial.ino
@@ -1,10 +1,7 @@
 #include <SoftwareSerial.h>
-#include <Wire.h>
 #include <PZEM004Tv30.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-
-#define VCC2 5
 
 SoftwareSerial nodemcu(2,3);
 PZEM004Tv30 pzem(11, 12); 
@@ -26,8 +23,6 @@ void setup()
 {
 Serial.begin(9600); 
 nodemcu.begin(9600);
-pinMode(VCC2, OUTPUT);
-digitalWrite(VCC2, HIGH); 
 display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
 display.clearDisplay();
 }
@@ -36,33 +31,12 @@ void loop()
 {
 if(nodemcu.available() == 0 )
 {
-      Serial.print("Voltage = ");
-    Serial.print(pzem.voltage());
-    sdata1 = pzem.voltage();
-    Serial.println(" V");
     
-    Serial.print("Current ");
-    Serial.print(pzem.current());
+    sdata1 = pzem.voltage();
     sdata2 = pzem.current();
-    Serial.println(" A");
- 
-    Serial.print("Power");
-    Serial.print(pzem.power());
     sdata3 = pzem.power();
-    Serial.println(" W");  
-
-    Serial.print("Energy");
-    Serial.print(pzem.energy());
     sdata4 = pzem.energy();
-    Serial.println(" kWh"); 
-
-    Serial.print("Frequency");
-    Serial.print(pzem.frequency());
     sdata5 = pzem.frequency();
-    Serial.println(" Hz"); 
-
-    Serial.print("Power Factor");
-    Serial.println(pzem.pf());
     sdata6 = pzem.pf();
  
    cdata = cdata + sdata1+","+sdata2+","+sdata3+","+sdata4+","+sdata5+","+sdata6;
@@ -72,10 +46,7 @@ if(nodemcu.available() == 0 )
    idata = idata + "Current = " + sdata2 + "A";
    edata = edata + "Energy = " + sdata4 + "kWh";
    pdata = pdata + "Power = " + sdata3 + "kWh";
-   /*display.setCursor(0,0);
-   display.setTextSize(1);
-   display.setTextColor(WHITE);
-   display.println("Meter readings");*/
+
    display.setCursor(0,0);
    display.setTextSize(1);
    display.setTextColor(WHITE);
@@ -84,7 +55,7 @@ if(nodemcu.available() == 0 )
    display.println(pdata);
    display.println(edata);
    display.display();
-   //display.startscrollright(0x00,0x00);
+ 
    delay(1000); // 100 milli seconds
    display.clearDisplay();
    cdata = "";
